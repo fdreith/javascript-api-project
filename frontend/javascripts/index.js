2d8c1c2b40a65ec537bff9762d518c7eb9bdcc5d
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-13 16:16:01 -0700

    Implementing entry model

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   getEntries()
   const prompt = document.getElementById("prompt")
   const entriesDiv = document.getElementById("entries-div")
+  const notepad = document.getElementById("notepad")
 
   prompt.addEventListener("click", e => {
     getPrompt(e.target.name)
@@ -16,13 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json()
       })
       .then(function (data) {
-        entries = data
-        renderEntries()
+        let entries = data.map(entry => new Entry(entry))
+        sortEntries(entries)
+        renderEntries(entries)
       })
       .catch(errors => console.log(errors)) // do something with this?
   }
 
-  function renderEntries() {
+  function sortEntries(entries) {
     entries.sort(function (a, b) {
       let keyA = new Date(a.created_at),
         keyB = new Date(b.updated_at)
@@ -30,26 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
       if (keyA > keyB) return 1
       return 0
     })
+  }
+
+  function renderEntries(entries) {
     entries.forEach(entry => render(entry))
   }
 
   function render(entry) {
-    entriesDiv.innerHTML += template(entry)
-  }
-
-  function template(entry) {
-    // need to reformat date & would like to have mood listed
-    // stretch -- mouseover to view content
-    return `
-    <div class="card">
-      <div class="card-content">
-        <strong> ${entry.prompt.question}</strong>
-        <p>${entry.created_at}</p> 
-        <p>Written in ${entry.minutes}</p>
-        <p>${entry.content}</p>
-      </div>
-    </div>
-    `
+    console.log(entry)
+    entriesDiv.insertAdjacentHTML('afterbegin', entry.renderEntry())
   }
 
   function getPrompt(promptType) {
@@ -61,21 +52,32 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json()
       })
       .then(function (data) {
-        prompts = data
-        renderPrompt(promptType)
+        let prompts = data.map(prompt => new Prompt(prompt))
+        renderPrompt(prompts)
       })
       .catch(errors => console.log(errors)) // do something with this?
   }
 
   function renderPrompt(promptType) {
     let targetPrompts = prompts.filter(prompt => prompt.mood_id.toString() === promptType)
+    let randomPrompt = targetPrompts.random().question
     debugger
-    prompt.innerHTML = targetPrompts.random().question
-    renderNotepad()
-  }
-
-  function renderNotepad() {
-    console.log("notepad")
+    prompt.innerHTML = `
+      <div>
+        <h4>${randomPrompt}</h4>
+      </div>
+      <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea"></textarea>
+            <label for="textarea1">Journal Entry</label>
+            <input
+          </div>
+        </div>
+      </form>
+    </div>
+    `
   }
 
   Array.prototype.random = function () {
