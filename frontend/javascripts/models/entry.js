f1059d8ae78dfef4366ec2d5442723c32df57295
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-14 14:58:29 -0700

    rearraged the entrie info being rendered

--- frontend/javascripts/models/entry.js
+++ frontend/javascripts/models/entry.js
@@ -1,4 +1,7 @@
 class Entry {
+
+  // static all = []
+
   constructor(data) {
     this.id = data.id
     this.prompt = data.prompt
@@ -6,15 +9,21 @@ class Entry {
     this.minutes = data.minutes
     this.content = data.content
     this.created_at = data.created_at // want to modify how this looks date and time 
+    // this.save()
   }
+
+  // save() {
+  //   Entry.all.push(this)
+  // }
 
   renderEntry() {
     return `
     <div class="card">
       <div class="card-content">
+         <strong>${this.created_at}</strong> 
+         <br>
         <strong> ${this.prompt.question}</strong>
-        <p>${this.created_at}</p> 
-        <p>Minutes writing: ${this.minutes} </p>
+        <br>
         <p>${this.content}</p>
       </div>
     </div>
