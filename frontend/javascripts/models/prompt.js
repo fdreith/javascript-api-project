dac85aa0dcfe7f09343f4cd9cac276518372e9dc
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-14 19:17:14 -0700

    moved function to the eindex.js file because of separation of concerns

--- frontend/javascripts/models/prompt.js
+++ frontend/javascripts/models/prompt.js
@@ -6,43 +6,4 @@ class Prompt {
     this.minutes = data.minutes
   }
 
-  renderPromptForm() {
-    return `
-      <div>
-        <h4>${this.question}</h4>
-      </div>
-      <div class="row">
-      <form class="prompt-form" id="prompt-form>
-        <div class="row">
-          <div class="input-field prompt-form">
-            <textarea id="textarea1" class="materialize-textarea"></textarea>
-            <label for="textarea1">Journal Entry</label>
-            <input type="submit" id="submit" name="submit"
-          </div>
-        </div>
-      </form>
-    </div>
-    `
-  }
-
-  // renderPromptForm() {
-  //   return `
-  //     <div>
-  //       <h4>${this.question}</h4>
-  //     </div>
-  //     <div class="row">
-  //     <form class="col s12">
-  //       <div class="row">
-  //         <div class="input-field col s12">
-  //           <textarea id="textarea1" class="materialize-textarea"></textarea>
-  //           <label for="textarea1">Journal Entry</label>
-  //           <input type="submit" id="submit" name="submit"
-  //         </div>
-  //       </div>
-  //     </form>
-  //   </div>
-  //   `
-  // }
-
 }
-
