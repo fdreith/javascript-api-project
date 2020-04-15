a0e414328ec36bbea13301ca72874fcd2f63461b
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-15 15:45:28 -0700

    changing ids and divs around delete button

--- frontend/javascripts/models/entry.js
+++ frontend/javascripts/models/entry.js
@@ -19,9 +19,13 @@ class Entry {
   renderEntry() {
     return `
     <div class="card">
+      <div class="delete-div>
+         <button class = "btn-flat waves-effect waves-teal disabled" >
+            <i id=${this.id} class = "material-icons right">close</i></button></td>
+      </div>
       <div class="card-content">
-         <strong>${this.created_at}</strong> 
-         <br>
+        <strong>${this.created_at}</strong> 
+        <br>
         <strong> ${this.prompt.question}</strong>
         <br>
         <p>${this.content}</p>
