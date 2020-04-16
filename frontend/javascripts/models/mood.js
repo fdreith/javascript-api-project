40074e28933d61e834646845dfa43bb818b47e95
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-16 13:28:31 -0700

    deleted render entry-- redundant

--- frontend/javascripts/models/mood.js
+++ frontend/javascripts/models/mood.js
@@ -7,4 +7,23 @@ class Mood {
     this.entries = data.entries
   }
 
+  renderMoodEntry() {
+    debugger
+    return `
+    <div class="card">
+      <div class="delete-div">
+         <button class = "btn-flat waves-effect waves-teal right">
+            <i id="${this.id}" class = "material-icons right">close</i></button>
+      </div>
+      <div class="card-content">
+        <strong>${this.created_at}</strong> 
+        <br>
+        <strong> ${this.prompt.question}</strong>
+        <br>
+        <p>${this.content}</p>
+      </div>
+    </div>
+    `
+  }
+
 }
