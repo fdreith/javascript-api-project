26c39c48c2d70cd56f01ca8704864dbcd6b61389
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-16 13:30:12 -0700

    saving prompts

--- frontend/javascripts/models/prompt.js
+++ frontend/javascripts/models/prompt.js
@@ -1,8 +1,15 @@
 class Prompt {
+  static all = []
+
   constructor(data) {
     this.id = data.id
     this.question = data.question
     this.mood = data.mood
+    this.save()
+  }
+
+  save() {
+    Prompt.all.push(this)
   }
 
 }
